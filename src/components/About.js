8edc5cadb9c37843369c7e1fda5b24e061d6fcba
Akhil-Ak-04 <pekalaakhil4@gmail.com>
2025-06-18 11:10:50 +0530

    updated about

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,7 @@ export default function About() {
     <section className="max-w-3xl mx-auto py-16 px-4 text-center">
       <h2 className="text-3xl font-bold mb-4">About Me</h2>
       <p className="text-gray-700 text-lg">
-        Detail-oriented UI Developer with 3.3 years of experience in designing and developing responsive, user-centric websites. Skilled in HTML, CSS, JavaScript, React JS, and modern frameworks to deliver visually appealing and high-performance interfaces. Proven ability to work independently and in cross-functional teams to enhance digital experiences, streamline development, and reduce costs.
+        Detail-oriented UI Developer with 3.5 years of experience in designing and developing responsive, user-centric websites. Skilled in HTML, CSS, JavaScript, React JS, and modern frameworks to deliver visually appealing and high-performance interfaces. Proven ability to work independently and in cross-functional teams to enhance digital experiences, streamline development, and reduce costs.
       </p>
     </section>
     </>
